61728a965ad34b2b578c1ccb6fefe0352c684060
Alkım Ercan <alkimtr34@gmail.com>
2025-04-30 22:40:57 +0300

    update

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { NAV_LINKS } from "@/constans";
+import Link from "next/link";
+import { MenuIcon, User } from "lucide-react";
+import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav>
+      <div>
+        <Link href={""}>
+          <span>Candy textile</span>
+        </Link>
+        <ul>
+          {NAV_LINKS.map((link) => (
+            <Link key={link.key} href={link.href}>{link.label}</Link>
+          ))}
+        </ul>
+        <div>
+          <Button>
+            <User>Login</User>
+          </Button>
+        </div>
+        <MenuIcon></MenuIcon>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
