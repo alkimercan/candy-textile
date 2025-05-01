53b4c9fa4e1ce155490af7891564e634a82c828d
Alkım Ercan <alkimtr34@gmail.com>
2025-05-02 02:18:13 +0300

    update

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,27 +1,42 @@
-import React from "react";
 import { NAV_LINKS } from "@/constans";
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
-import { MenuIcon, User } from "lucide-react";
-import { Button } from "../ui/button";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <Link href={""}>
-          <span>Candy textile</span>
+    <nav className="border border-b shadow-sm">
+      <div className="flex justify-between items-center px-3 py-3 relative z-30 container">
+        <Link
+          data-aos="fade-down"
+          data-aos-once="true"
+          data-aos-delay={100}
+          href="/"
+          className="font-semibold font-pacifiko text-xl"
+        >
+          <Image
+            alt=""
+            src={"/img/candy.png"}
+            width={500}
+            height={500}
+            className="mx-auto transform p-6 rounded-full group-hover:scale-105:"
+          ></Image>
         </Link>
-        <ul>
+        <MenuIcon className="lg:hidden inline-block cursor-pointer w-8 h-8 text-green-600" />
+        <ul className="hidden lg:flex h-full gap-10">
           {NAV_LINKS.map((link) => (
-            <Link key={link.key} href={link.href}>{link.label}</Link>
+            <Link
+              key={link.key}
+              href={link.href}
+              data-aos="fade-down"
+              data-aos-once="true"
+              data-aos-delay={link.delay}
+              className="font-popins flex items-center cursor-pointer hover:font-bold text-slate-700 hover:text-slate-950"
+            >
+              {link.label}
+            </Link>
           ))}
         </ul>
-        <div>
-          <Button>
-            <User>Login</User>
-          </Button>
-        </div>
-        <MenuIcon></MenuIcon>
       </div>
     </nav>
   );
